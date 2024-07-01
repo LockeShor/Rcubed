f35917a4dd051303b3fa1d193deaf62e85d9ade3
LockeShor <75901583+LockeShor@users.noreply.github.com>
2024-07-01 15:33:17 -0400

    probably not functional

--- fit.py
+++ fit.py
@@ -16,12 +16,13 @@ for line in data:
 #prepare each row into the inputs and outputs of the function to fit
 inputs = [
     Kv,
-    Ka
+    Ka,
+    Ks
 ]
 outputs = Error
 
 #used for printing only
-varnames = ['a', 'm', 'h', 'n', 'j', 'k']
+varnames = ['a', 'm', 'h', 'n', 'j', 'k', 'o', 'p']
 # 
 # a: overall steepness, around 810 (may be redundant with n2 & m2)
 # m: steepness in x direction, around 0.4
@@ -29,29 +30,32 @@ varnames = ['a', 'm', 'h', 'n', 'j', 'k']
 # n: steepness in y direction, around 1.13
 # j: center of y direction, around 0.002
 # k: center of z direction, around 0.195
+# o: steepness in w direction, around 1
+# p: center of w direction, around 0.001
 
 #function to fit (maybe put abs around it because error can be negative?)
-def f(x, a, m, h, n, j, k):
-    return a * np.sqrt(m*((x[0] - h)**2) + n*((x[1] - j)**2)) + k
+def f(x, a, m, h, n, j, k, o, p):
+    return a * np.sqrt(m*((x[0] - h)**2) + n*((x[1] - j)**2) + o*((x[2] - p)**2)) + k
 
 #fit and print results for each independent variable
-popt, pcov, infodict, mesg, ier = curve_fit(f, inputs, outputs, p0=[810, 0.4, 0.016, 1.13, 0.002, 0.195], full_output=True)
+popt, pcov, infodict, mesg, ier = curve_fit(f, inputs, outputs, p0=[810, 0.4, 0.016, 1.13, 0.002, 0.195, 1, 0.004], full_output=True, maxfev=10000)
 for i in range(len(popt)):
     print('{0} = {1:f}'.format(varnames[i], popt[i]))
 
 #give the whole equation for use in desmos or other
-print("Final Equation: z = {0}sqrt({1}((x-{2})^2) + {3}((y-{4})^2)) + {5}".format(popt[0], popt[1], popt[2], popt[3], popt[4], popt[5]))
-# print(pcov)
+# print("Final Equation: z = {0}sqrt({1}((x-{2})^2) + {3}((y-{4})^2)) + {5}".format(popt[0], popt[1], popt[2], popt[3], popt[4], popt[5]))
+print("Final Equation: z = {0}sqrt({1}((x-{2})^2) + {3}((y-{4})^2) + {6}((w-{7})^2)) + {5}".format(popt[0], popt[1], popt[2], popt[3], popt[4], popt[5], popt[6], popt[7]))
+#print(pcov)
 # print(infodict)
 # print(mesg)
 # print(ier)
 
 #fitted = the final regressed function
 def fitted(x):
-    return f(x, *popt)
+    return abs(f(x, *popt))
 
 #find the minimum point of the function - lowest error
-min_optimized = minimize(fitted, [0,0], method="Nelder-Mead")
+min_optimized = minimize(fitted, [0,0,0], method="Nelder-Mead")
 
 min_input_vector = min_optimized.x
 min_output = fitted(min_input_vector)
@@ -59,4 +63,4 @@ min_output = fitted(min_input_vector)
 print("Minimum Point: {0} at {1}".format(min_output, min_input_vector))
 
 #print relevant results in a more readable format
-print("\n-------------------\nBest Fit for Roadrunner:\nKv = {0}\nKa = {1}\nKs = {2}\nError = {3}".format(min_input_vector[0], min_input_vector[1], "Not Implemented", min_output))
+print("\n-------------------\nBest Fit for Roadrunner:\nKv = {0}\nKa = {1}\nKs = {2}\nError = {3}".format(min_input_vector[0], min_input_vector[1], min_input_vector[2], min_output))
