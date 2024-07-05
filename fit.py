ce958447cd5881da3bd896268452e2f382307fa9
LockeShor <75901583+LockeShor@users.noreply.github.com>
2024-07-05 13:02:21 -0400

    cleanup code and add comments

--- fit.py
+++ fit.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import curve_fit, minimize
+from scipy.optimize import curve_fit
 
 #import roadrunner test data
 kvkadata = open('kv&ka.csv', 'r').read().splitlines()[1:]
@@ -12,43 +12,38 @@ for line in kvkadata:
     Ka.append(float(values[1]))
     Stdev.append(float(values[2]))
 
-#prepare each row into the inputs and outputs of the function to fit
+#prepare each row into the inputs and outputs of the first function to fit
 inputs = [
     Kv,
     Ka
 ]
 output = Stdev
 
-#used for printing only
-varnames = ['a', 'h', 'b', 'k', 'c']
-# a: steepness in x direction, around 0.4
-# h: center of x direction, around 0.016
-# b: steepness in y direction, around 1.13
-# k: center of y direction, around 0.002
-# c: center of z direction, around 0.195
 
-#function to fit (maybe put abs around it because error can be negative?)
+# a: steepness in x direction, around 1500
+# h: center of x direction, around 0.016
+# b: steepness in y direction, around 577
+# k: center of y direction, around 0.002
+# c: center of z direction, around 0.5
+#first function to fit - returns best Ka and Kv values
 def f1(xy, a, h, b, k, c):
     x,y = xy
     return (a*np.abs(x-h))+(b*np.abs(y-k))+c
+#TODO: implement conical fitting function (as long as that better aligns to the additional data we gather)
 
-#fit and print results for each independent variable
+#fit the data and get the best values for Ka and Kv, along with extra info
 popt, pcov, infodict, mesg, ier = curve_fit(f1, inputs, output, p0=[1500, 0.016, 577, 0.002, 0.5], full_output=True)
 
-finalKv = popt[1]
-finalKa = popt[3]
-
-# for i in range(len(popt)):
-#     print('{0} = {1:f}'.format(varnames[i], popt[i]))
+finalKv = popt[1] #index 1 = h value = best Kv
+finalKa = popt[3] #index 3 = k value = best Ka
 
 #give the whole equation for use in desmos or other
-print("First Equation: stdev = {0}|Kv-{1}|+{2}|Ka-{3}|+{4}".format(popt[0], popt[1], popt[2], popt[3], popt[4]))
-# print(pcov)
-# print(infodict)
-# print(mesg)
-# print(ier)
+print("Kv and Ka Equation: stdev = {0}|Kv-{1}|+{2}|Ka-{3}|+{4}".format(popt[0], popt[1], popt[2], popt[3], popt[4]))
 
-#print("Minimum Point: {0} at {1}".format(popt[1],popt[3]))
+
+# TODO:
+# We need to stop at this point and output the optimal Kv and Ka, so that the user can gather the Ks data using those values
+# Not sure exactly how we want to do this, we may just want two scripts
 
 ksdata = open('ks.csv', 'r').read().splitlines()[1:]
 
@@ -59,17 +54,15 @@ for line in ksdata:
     Ks.append(float(values[0]))
     Error.append(float(values[1]))
 
+#second function to fit - returns best Ks value (simple linear regression)
 def f2(x,m,b):
     return m*(x-b)
 
+#fit and print the equation for Ks
 popt2, pcov2 = curve_fit(f2, Ks, Error)
-print("Second Equation: error = {0}(Ks-{1})".format(popt2[0], popt2[1]))
+print("Ks Equation: Error = {0}(Ks-{1})".format(popt2[0], popt2[1]))
 
-finalKs = popt2[1]
+finalKs = popt2[1] #index 1 = b value = best Ks
 
-print("Final Values: Kv = {0}, Ka = {1}, Ks = {2}".format(round(finalKv,6), round(finalKa,6), round(finalKs,6)))
-
-
-
-#print relevant results in a more readable format
-#print("\n-------------------\nBest Fit for Roadrunner:\nKv = {0}\nKa = {1}\nKs = {2}\nError = {3}".format(min_input_vector[0], min_input_vector[1], "Not Implemented", min_output))
+#print results in a more readable format
+print("{3}\nOptimal Values for RoadRunner:\nKv = {0}\nKa = {1}\nKs = {2}".format(finalKv, finalKa, finalKs, "-"*40))
